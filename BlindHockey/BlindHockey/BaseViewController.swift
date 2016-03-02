f2d59ea2d0f2284aa63412f23679532d7666605f
Tomoki-n <ariaqua713.f7z@gmail.com>
2016-03-02 18:01:48 +0900

    [WIP] ボールを消す関数

---
+++ BlindHockey/BlindHockey/BaseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BaseViewController.swift
+//  BlindHockey
+//
+//  Created by Nishinaka Tomoki on 2016/02/20.
+//  Copyright © 2016年 Nishinaka Tomoki. All rights reserved.
+//
+
+import Foundation
